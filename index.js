1dde6e5f4b85d0fef221a4f07a7ca4d320993fb2
Keeley <keeleyduffey@gmail.com>
2019-05-31 13:47:51 -0600

    Adds js file with high level function outlines

---
+++ index.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const recipeSearchURL = '',
+  recipeApiKey = '',
+  articleSearchURL = '',
+  articleApiKey = '',
+  videoSearchURL = '',
+  videoApiKey = '';
+
+
+
+function getResults () {
+  // calls fetch requests for each api
+}
+
+
+function displayRecipes (responseJSON) {
+  // renders recipes to DOM
+}
+
+function displayArticles (responseJSON) {
+  // renders articles to DOM
+}
+
+function displayVideo (responseJSON) {
+  // renders video to DOM
+}
+
+function submitForm() {
+  
+}
+
+$(function() {
+  submitForm();
+});
